e879be484679bfea94fdf7e97556bcb3d501493c
Haroon <harooncloud4@gmail.com>
2024-11-13 07:25:44 +0000

    added dataset

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 18:56:10 2024
+
+@author: Haroon
+"""
+print("hello")
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset =pd.read_csv('Salary_Data.csv')
