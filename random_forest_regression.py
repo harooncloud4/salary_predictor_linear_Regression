7717c7dabfc494ffa380a47bf3f3b135c54ede37
Haroon <harooncloud4@gmail.com>
2024-11-28 00:25:19 +0000

    Update random_forest_regression.py

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -20,7 +20,7 @@ y = dataset.iloc[:, -1].values
 #Training the Random Forest Regression model on the whole dataset
 from sklearn.ensemble import RandomForestRegressor
 
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0  )
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0  ) 
 regressor.fit(X,y)
 
 #PRedicting new results
