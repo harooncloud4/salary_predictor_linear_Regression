2ed94134d172c379af879ea315d490e748247321
Haroon <harooncloud4@gmail.com>
2024-11-30 20:38:03 +0000

    added svr and xgbost

--- models.py
+++ models.py
@@ -9,6 +9,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.svm import SVR
+
+
 from sklearn.metrics import r2_score, mean_squared_error
 
 
@@ -40,6 +44,19 @@ def train_decision_tree(X_train,y_train):
     regressor = DecisionTreeRegressor(random_state = 0)
     regressor.fit(X_train,y_train)
     return regressor
+
+# Train Support Vector Regression (SVR)
+def train_svr(X_train, y_train):
+   
+    svr = SVR(kernel='rbf', C=1.0, epsilon=0.1)
+    svr.fit(X_train, y_train)
+    return svr
+
+# Train Gradient Boosting (XGBoost)
+def train_xgboost(X_train, y_train):
+    xgb = XGBRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=0)
+    xgb.fit(X_train, y_train)
+    return xgb
 
 # Evaluate Model
 def evaluate_model(y_true, y_pred):
